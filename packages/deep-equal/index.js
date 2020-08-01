67890f4d2b8022e5097d9ad38d92d833f08c310f
wareset <git@wareset.com>
2020-08-01 17:53:58 +0500

    Change methods

--- packages/deep-equal/index.js
+++ packages/deep-equal/index.js
@@ -1,11 +1,11 @@
-const typed = require('@wareset/typed');
-
 module.exports = function equal(a, b, deep = true) {
   if (a === b) return true;
-  if (typeof a !== 'object') return a !== a && b !== b;
+  if (!a || !b || typeof a !== 'object' || typeof b !== 'object') {
+    return a !== a && b !== b;
+  }
 
-  const proto = typed(a);
-  if (!typed(b, proto)) return false;
+  const proto = Object.getPrototypeOf(a);
+  if (proto !== Object.getPrototypeOf(b)) return false;
 
   const deep2 = typeof deep !== 'number' ? deep : deep <= 0 ? 0 : deep--;
 
@@ -14,22 +14,42 @@ module.exports = function equal(a, b, deep = true) {
     case Object.prototype:
       keys = Object.keys(a);
       if (keys.length !== Object.keys(b).length) return false;
-      return !keys.some(k => {
-        return deep ? !equal(a[k], b[k], deep2) : a[k] !== b[k];
-      });
+
+      for (let k = keys.length; k-- > 0; undefined) {
+        if (!Object.prototype.hasOwnProperty.call(b, keys[k])) return false;
+      }
+
+      if (!deep) {
+        for (let k = keys.length; k-- > 0; undefined) {
+          if (a[keys[k]] !== b[keys[k]]) return false;
+        }
+      } else {
+        for (let k = keys.length; k-- > 0; undefined) {
+          if (!equal(a[keys[k]], b[keys[k]], deep2)) return false;
+        }
+      }
+      return true;
 
     case Array.prototype:
       if (a.length !== b.length) return false;
-      return !a.some((v, k) => {
-        return deep ? !equal(a[k], b[k], deep2) : a[k] !== b[k];
-      });
+      if (!deep) {
+        for (let k = a.length; k-- > 0; undefined) {
+          if (a[k] !== b[k]) return false;
+        }
+      } else {
+        for (let k = a.length; k-- > 0; undefined) {
+          if (!equal(a[k], b[k], deep2)) return false;
+        }
+      }
+      return true;
 
     case Map.prototype:
       if (a.size !== b.size) return false;
-      for (const [k, v] of a) {
-        if (!b.has(k) || deep ? !equal(v, b.get(k), deep2) : v !== b.get(k)) {
-          return false;
-        }
+      for (const [k] of a) if (!b.has(k)) return false;
+      if (!deep) {
+        for (const [k, v] of a) if (v !== b.get(k)) return false;
+      } else {
+        for (const [k, v] of a) if (!equal(v, b.get(k), deep2)) return false;
       }
       return true;
 
@@ -44,12 +64,16 @@ module.exports = function equal(a, b, deep = true) {
     default:
       if (ArrayBuffer.isView(a)) {
         if (a.length !== b.length) return false;
-        for (let i = a.length; i !== 0; i--) if (a[i] !== b[i]) return false;
+        for (let k = a.length; k-- > 0; undefined) {
+          if (a[k] !== b[k]) return false;
+        }
         return true;
       }
+
       if (a.valueOf !== Object.prototype.valueOf) {
         return a.valueOf() === b.valueOf();
       }
+
       if (a.toString !== Object.prototype.toString) {
         return a.toString() === b.toString();
       }
